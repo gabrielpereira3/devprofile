8386ded2bc574d0a1b48929ac938513e08ac3e3e
Gabriel <gabriel.soares14563@gmail.com>
2022-09-19 09:39:30 -0300

    Usando fontes do google (sucesso) e splash screen enquanto elas carregam (ainda não testado)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 import { Home } from './src/pages/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Anton_400Regular } from '@expo-google-fonts/anton';
+import * as SplashScreen from 'expo-splash-screen';
 
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Anton_400Regular,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,7 +8,8 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
   font-size: 24px;
-  font-weight: bold;
+  /* font-weight: bold; */
   color: ${({ theme }) => theme.colors.light};
 `;
