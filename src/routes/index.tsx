5026e90d184a267a2f9bb4cfeb818440fe5a9123
Gabriel <gabriel.soares14563@gmail.com>
2022-09-21 13:23:41 -0300

    Rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
 
 export const Routes: React.FunctionComponent = () => {
-  return <AuthRoutes />;
+  const { user } = useAuth();
+
+  return user?.id ? <AppRoutes /> : <AuthRoutes />;
 };
