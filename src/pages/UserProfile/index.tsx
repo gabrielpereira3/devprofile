30b5de0395a92ab76b347269b75066707c3195af
Gabriel <gabriel.soares14563@gmail.com>
2022-09-22 13:41:46 -0300

    Perfil do usuário

---
+++ src/pages/UserProfile/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Content,
+  EmailData,
+  EmailTitle,
+  GoBackButton,
+  Header,
+  HeaderTitle,
+  HeaderTop,
+  Icon,
+  NameData,
+  NameTitle,
+  PhotoButton,
+  PhotoContainer,
+  UserAvatar,
+  UserEmailDetail,
+  UserNameDetail,
+} from './styles';
+import avatarDefault from '../../assets/avatar01.png';
+import { useAuth } from '../../context/AuthContext';
+import { Button } from '../../components/Form/Button';
+
+interface ScreenNavigationProp {
+  goBack: () => void;
+}
+
+export const UserProfile: React.FunctionComponent = () => {
+  const { user } = useAuth();
+  const { goBack } = useNavigation<ScreenNavigationProp>();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <GoBackButton onPress={goBack}>
+            <Icon name="chevron-left" />
+          </GoBackButton>
+          <HeaderTitle>Seu perfil</HeaderTitle>
+        </HeaderTop>
+
+        <PhotoContainer>
+          <UserAvatar
+            source={user.avatar_url ? { uri: user.avatar_url } : avatarDefault}
+          />
+
+          <PhotoButton>
+            <Icon name="camera" />
+          </PhotoButton>
+        </PhotoContainer>
+      </Header>
+
+      <Content>
+        <UserNameDetail>
+          <NameTitle>Name</NameTitle>
+          <NameData>{user.name}</NameData>
+        </UserNameDetail>
+        <UserEmailDetail>
+          <EmailTitle>Email</EmailTitle>
+          <EmailData>{user.email}</EmailData>
+        </UserEmailDetail>
+        <Button title="Editar dados do perfil" onPress={() => {}} />
+        <Button title="Trocar senha" onPress={() => {}} />
+      </Content>
+    </Container>
+  );
+};
